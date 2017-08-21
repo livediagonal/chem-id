b16471cf4c40536d1a3d1b6bfd31d42eeee9d1bf
Anthony D'Addeo <amdaddeo@gmail.com>
2017-08-20 22:19:46 -0500

    Setup namespaced layouts

--- app/controllers/frontend/base_controller.rb
+++ app/controllers/frontend/base_controller.rb
@@ -2,6 +2,8 @@ module Frontend
   class BaseController < ActionController::Base
     protect_from_forgery with: :null_session
 
+    layout 'frontend/layouts/application'
+
     before_action :authenticate_user!
   end
 end
