1f849e85e6a5ae2928aa9be03a903a40641cbfb4
Anthony D'Addeo <amdaddeo@gmail.com>
2017-08-22 13:47:25 -0500

    Add timestamps to samples tables

--- db/migrate/20170821015737_create_samples.rb
+++ db/migrate/20170821015737_create_samples.rb
@@ -4,6 +4,8 @@ class CreateSamples < ActiveRecord::Migration[5.1]
       t.string :name
       t.text :notes
       t.references :user, foreign_key: true
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20170821020008) do
     t.string "name"
     t.text "notes"
     t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_samples_on_user_id"
   end
 
