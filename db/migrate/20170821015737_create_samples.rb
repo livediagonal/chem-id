2231437bb8ff91855fbd5cf19a27aeab794b8559
Anthony D'Addeo <amdaddeo@gmail.com>
2017-08-22 14:02:25 -0500

    Expand samples table

--- db/migrate/20170821015737_create_samples.rb
+++ db/migrate/20170821015737_create_samples.rb
@@ -2,6 +2,10 @@ class CreateSamples < ActiveRecord::Migration[5.1]
   def change
     create_table :samples do |t|
       t.string :name
+      t.string :sample_type
+      t.string :test_platform
+      t.string :test_type
+      t.timestamp :test_time
       t.text :notes
       t.references :user, foreign_key: true
 
